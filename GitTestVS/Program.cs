14017fafbec48c90d3594ac991a5899776d0f6d9
Renat Doroshenko <r290399@gmail.com>
2018-11-25 22:17:41 +0200

    added second ReadKey();

--- GitTestVS/Program.cs
+++ GitTestVS/Program.cs
@@ -14,6 +14,7 @@ namespace GitTestVS
             Console.WriteLine("New string");
             Console.WriteLine("Hello World!");            
             Console.ReadKey();
+            Console.ReadKey();
 
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
